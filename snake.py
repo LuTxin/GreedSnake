7b7e39902a589efdc2abe729de371474dd0048d7
Lu Zhang <lu.zhang@aofl.com>
2020-08-11 00:39:43 -0700

    Added meat

--- snake.py
+++ snake.py
@@ -8,6 +8,7 @@ import termios
 
 from queue import Queue
 from threading import Thread
+from random import randrange
 
 class Vector:
 	'Vector'
@@ -38,24 +39,38 @@ class Board:
 
 class Snake:
 	'class for snake'
+	increase = False
 	speed = Vector(-1, 0)
 	def __init__(self, bodyList):
 		self.bodyList = bodyList
-	def updatePosition(self):
+	def UpdatePosition(self):
 		print(self.speed.x)
 		print(self.speed.y)
 
 		newHead = Vector(self.bodyList[0].x + self.speed.x, self.bodyList[0].y + self.speed.y)
 		self.bodyList.insert(0, newHead)
-		self.bodyList.pop(len(self.bodyList) - 1)
+		if(self.increase == False):
+			self.bodyList.pop(len(self.bodyList) - 1)
+		else:
+			self.increase = False
+
 		for i in range(1, len(self.bodyList)):
 			if(newHead.x == self.bodyList[i].x and newHead.y == self.bodyList[i].y):
 				return False
 		return True
 
+	def CheckIncrease(self, meatPosition):
+		newHead = Vector(self.bodyList[0].x + self.speed.x, self.bodyList[0].y + self.speed.y)
+		if(newHead.x == meatPosition.x and newHead.y == meatPosition.y):
+			self.increase = True
+			return True
+		return False
+
 class Main:
 	'Main'
 
+	currentInput = 'a'
+	meat = Vector(10, 10)
 	alive = True
 	count = 0
 	refreshSpeed = 1
@@ -74,18 +89,34 @@ class Main:
 		fifthBody = Vector(int(self.board.width/2) + 3, int(self.board.height/2))
 		self.snake = Snake([firstBody, secondBody, thirdBody, forthBody, fifthBody])
 
-
 	def Start(self):
 		self.scheduler.run()
 
 	def Refresh(self, sc, q):
-		self.alive = self.snake.updatePosition()
+		self.GetSpeed()
+		needNewMeat = self.snake.CheckIncrease(self.meat);
+		self.alive = self.snake.UpdatePosition()
 		self.alive = self.alive and self.Judge()
+		if(needNewMeat):
+			self.NewMeat()
 		self.Render()
 		if(self.alive == False):
 			return
 		self.count += self.refreshSpeed
 		self.schedulerEvent = self.scheduler.enter(self.refreshSpeed, 1, self.Refresh, (sc, q))
+
+	def NewMeat(self):
+		self.meat.x = randrange(0, self.board.width)
+		self.meat.y = randrange(0, self.board.height)
+		while(not self.CheckMeatLegit()):
+			self.meat.x = randrange(0, self.board.width)
+			self.meat.y = randrange(0, self.board.height)
+
+	def CheckMeatLegit(self):
+		for body in self.snake.bodyList:
+			if(self.meat.x == body.x and self.meat.y == body.y):
+				return False
+		return True
 
 	def Judge(self):
 		if(self.snake.bodyList[0].x >= self.board.width or self.snake.bodyList[0].x < 0 
@@ -107,6 +138,9 @@ class Main:
 			self.board.SetValue(body.y, body.x, 1)
 		self.board.SetValue(self.snake.bodyList[0].y, self.snake.bodyList[0].x, 0)
 
+		if(self.meat.x >= 0 and self.meat.y >= 0):
+			self.board.SetValue(self.meat.y, self.meat.x, '6')
+
 		if(self.alive == False):
 			self.board.SetValue(int(self.board.height/2), int(self.board.width/2), '菜')
 			self.board.SetValue(int(self.board.height/2), int(self.board.width) - 2, '')
@@ -117,23 +151,27 @@ class Main:
 				print(self.board.boardValue[i][j], end = '')
 			print("")
 
-	def InputReceived(self, input):
-		if(input == 'w'):
+	def GetSpeed(self):
+		if(self.currentInput == 'w'):
 			if(self.snake.speed.y != 1):
 				self.snake.speed.x = 0
 				self.snake.speed.y = -1
-		elif(input == 's'):
+		elif(self.currentInput == 's'):
 			if(self.snake.speed.y != -1):
 				self.snake.speed.x = 0
 				self.snake.speed.y = 1
-		elif(input == 'a'):
+		elif(self.currentInput == 'a'):
 			if(self.snake.speed.x != 1):
 				self.snake.speed.x = -1
 				self.snake.speed.y = 0
-		elif(input == 'd'):
+		elif(self.currentInput == 'd'):
 			if(self.snake.speed.x != -1):
 				self.snake.speed.x = 1
 				self.snake.speed.y = 0
+
+	def InputReceived(self, input):
+		self.currentInput = input;
+		
 
 def GameLoop(main, int_q):
 	main.schedulerEvent = main.scheduler.enter(main.refreshSpeed, 1, main.Refresh, (main.scheduler, int_q))
